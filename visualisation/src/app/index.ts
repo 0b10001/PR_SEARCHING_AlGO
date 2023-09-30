070a2228d974e2065ce8a8fbb61ea193a1536a11
Teboho Molise <114547885+0b10001@users.noreply.github.com>
2023-09-30 21:39:42 +0000

    starting from scratch

--- visualisation/src/app/index.ts
+++
@@ -1,7 +0,0 @@
-import { BFS } from "./algorithms/BFS";
-import { DFS } from "./algorithms/DFS";
-import { dijkstra } from "./algorithms/dijkstra";
-import { generateRandomMaze } from "./maze/randomMaze";
-import { generateRecursiveMaze } from "./maze/recursiveMaze";
-
-export { BFS, DFS, dijkstra, generateRandomMaze, generateRecursiveMaze };
