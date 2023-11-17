3fc59b438d4497339b26a91f4ed24d91c9fddb1b
megannyabvudzi <105789666+megannyabvudzi@users.noreply.github.com>
2023-11-17 23:49:49 +0200

    Update jest.config.js
    Signed-off-by: megannyabvudzi <105789666+megannyabvudzi@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   collectCoverage: true,
   coverageReporters: ['text', 'cobertura'],
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.js'],
-  collectCoverageFrom: ['**/*.js', '!**/node_modules/**'],
-  setupFiles: ["<rootDir>/jest.setup.js"],
 }
